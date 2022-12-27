cf3a9ffae57e76eb721d1472c746603dce53a6fc
NurayMercan26 <109817919+NurayMercan26@users.noreply.github.com>
2022-12-27 20:53:35 +0300

    Update SpartanGetRequests.java

--- src/main/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/main/java/com/cydeo/day2/SpartanGetRequests.java
@@ -15,7 +15,7 @@ public class SpartanGetRequests {
 //    Then status code must 200
 //    And response Content Type must be application/json
 //    And response body should include spartan result
-    String baseUrl = "http://54.175.31.71:8000";
+    String baseUrl = "http://.175.31.71:8000";
     @Test
     public void test1(){
 
